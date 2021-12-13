b133a6cb0eb37981ceeaee7443d32347a0e31358
Inna <i.m.makhova@gmail.com>
2021-12-13 14:56:05 +0200

    Добавлен компонент карточка квеста

---
+++ src/components/home/components/quests-catalog/components/quest-card.jsx
@@ -0,0 +1,40 @@
+import * as S from '../quests-catalog.styled';
+import { ReactComponent as IconPerson } from '../../../../../assets/img/icon-person.svg';
+import { ReactComponent as IconPuzzle } from '../../../../../assets/img/icon-puzzle.svg';
+import { AppRoute } from '../../../../../const';
+
+const QuestCard = (quest) => {
+  const { id, title, previewImg, level, peopleCount } = quest;
+
+  return (
+    <S.QuestItem>
+      <S.QuestItemLink to={`${AppRoute.Quest}/${id}`}>
+        <S.Quest>
+          <S.QuestImage
+            src={previewImg}
+            width="344"
+            height="232"
+            alt={`квест ${title}`}
+          />
+
+          <S.QuestContent>
+            <S.QuestTitle>{title}</S.QuestTitle>
+
+            <S.QuestFeatures>
+              <S.QuestFeatureItem>
+                <IconPerson />
+                {`${peopleCount[0]}–${peopleCount[peopleCount.length-1]} чел`}
+              </S.QuestFeatureItem>
+              <S.QuestFeatureItem>
+                <IconPuzzle />
+                {level}
+              </S.QuestFeatureItem>
+            </S.QuestFeatures>
+          </S.QuestContent>
+        </S.Quest>
+      </S.QuestItemLink>
+    </S.QuestItem>
+  );
+};
+
+export default QuestCard;
