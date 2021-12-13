1c6103fd241630f9cedffef46430d759ded849ca
Inna <i.m.makhova@gmail.com>
2021-12-13 16:10:57 +0200

    Добавлен служебный пропс ключ

--- src/components/home/components/quests-catalog/components/quest-list/quest-list.jsx
+++ src/components/home/components/quests-catalog/components/quest-list/quest-list.jsx
@@ -2,7 +2,7 @@ import QuestCard from '../quest-card/quest-card';
 import * as S from '../../quests-catalog.styled';
 
 const QuestList = (quests) => {
-  const questCards = quests.forEach((quest) => <QuestCard quest={quest} />);
+  const questCards = quests.forEach((quest) => <QuestCard quest={quest} key={quest.id}/>);
 
   return (
     <S.QuestsList>
