d5f575b574c0c6de68b159645e0eab49b46fe701
Inna <i.m.makhova@gmail.com>
2021-12-16 22:30:33 +0200

    Добавлена интерактивная карта на страницу контактов

---
+++ src/components/contacts/components/map/map.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react"
+import * as ol from "ol";
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import View from 'ol/View';
+import { fromLonLat } from 'ol/proj';
+import { Point } from 'ol/geom';
+import { Icon, Style } from 'ol/style';
+import { Feature } from 'ol';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+
+const MainCoordinates = {
+  Lon: 59.96813,
+  Lat: 30.31653
+}
+
+const Map = () => {
+
+  const coordinates = fromLonLat([MainCoordinates.Lat, MainCoordinates.Lon]);
+
+  const iconFeature = new Feature({
+    geometry: new Point(coordinates),
+    name: 'Escape Room',
+  });
+
+  const iconStyle = new Style({
+    image: new Icon({
+      crossOrigin: 'anonymous',
+      src: 'img/pin.png',
+      scale: 0.1,
+      anchor: [0.5, 1],
+    }),
+  });
+
+  iconFeature.setStyle(iconStyle);
+
+  const vectorSource = new VectorSource({
+    features: [iconFeature],
+  });
+
+  const vectorLayer = new VectorLayer({
+    source: vectorSource,
+  });
+
+
+  useEffect(() => {
+    new ol.Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      , vectorLayer],
+      view: new View({
+        center: coordinates,
+        zoom: 16
+      })
+    });
+  }, []);
+
+  return (
+      <div id='map'>
+      </div>
+  )
+}
+
+export default Map;
--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -1,6 +1,6 @@
 import { MainLayout, PageTitle, PageSubtext } from 'components/common/common';
-import contactsMap from 'assets/img/contacts-map.jpg';
 import * as S from './contacts.styled';
+import Map from './components/map/map';
 
 const Contacts = () => (
   <MainLayout>
@@ -40,13 +40,8 @@ const Contacts = () => (
             </S.ContactValue>
           </S.ContactsList>
 
-          <S.ContactsMap>
-            <S.ContactsMapImage
-              src={contactsMap}
-              alt="мы находимся по адресу Санкт-Петербург, Набережная реки Карповка, д 5"
-              width="649"
-              height="336"
-            />
+          <S.ContactsMap id='map'>
+            <Map />
           </S.ContactsMap>
         </S.Contacts>
       </S.ContentWrapper>
