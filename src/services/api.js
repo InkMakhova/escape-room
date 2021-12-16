37cf2d271a520df79dc022a96edb3c7aaece8549
Inna <i.m.makhova@gmail.com>
2021-12-16 22:25:09 +0200

    Увеличено время таймаута

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BACKEND_URL = 'http://localhost:3001/';
-const TIMEOUT = 1000;
+const TIMEOUT = 5000;
 
 export const createAPI = () => {
   const api = axios.create({
