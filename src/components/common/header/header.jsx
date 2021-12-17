6bb8e14e51d3b8c4b59e0ce5b026d9a51b6db6ad
Inna <i.m.makhova@gmail.com>
2021-12-17 11:18:57 +0200

    Рефакторинг активных состояний ссылок на страницы приложения, маршруты к страницам

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -1,42 +1,69 @@
 import logo from 'assets/img/logo.svg';
 import * as S from './header.styled';
-import { AppRoute } from '../../../const';
+import { AppRoute, Pages } from '../../../const';
+import { useLocation } from 'react-router-dom';
 
-const Header = () => (
-  <S.StyledHeader>
-    <S.HeaderWrapper>
-      <S.LogoLink to={AppRoute.Root}>
-        <S.Image src={logo} alt="Логотип Escape Room" width="134" height="50" />
-      </S.LogoLink>
+const Header = () => {
+  const location = useLocation();
 
-      <S.Navigation>
-        <S.Links>
-          <S.LinkItem>
-            <S.Link $isActiveLink to={AppRoute.Root}>
-              Квесты
-            </S.Link>
-          </S.LinkItem>
+  return (
+    <S.StyledHeader>
+      <S.HeaderWrapper>
+        <S.LogoLink to={AppRoute.Root}>
+          <S.Image src={logo} alt="Логотип Escape Room" width="134" height="50" />
+        </S.LogoLink>
 
-          <S.LinkItem>
-            <S.Link to="#">Новичкам</S.Link>
-          </S.LinkItem>
+        <S.Navigation>
+          <S.Links>
+            <S.LinkItem>
+              <S.Link
+                $isActiveLink={location.pathname === AppRoute.Root}
+                to={AppRoute.Root}
+              >
+                {Pages.Quests}
+              </S.Link>
+            </S.LinkItem>
 
-          <S.LinkItem>
-            <S.Link to="#">Отзывы</S.Link>
-          </S.LinkItem>
+            <S.LinkItem>
+              <S.Link
+                $isActiveLink={location.pathname === AppRoute.NewGamers}
+                to={AppRoute.NewGamers}
+              >{Pages.NewGamers}
+              </S.Link>
+            </S.LinkItem>
 
-          <S.LinkItem>
-            <S.Link to="#">Акции</S.Link>
-          </S.LinkItem>
+            <S.LinkItem>
+              <S.Link
+                $isActiveLink={location.pathname === AppRoute.Reviews}
+                to={AppRoute.Reviews}
+              >
+                {Pages.Reviews}
+              </S.Link>
+            </S.LinkItem>
 
-          <S.LinkItem>
-            <S.Link to={AppRoute.Contacts}>Контакты</S.Link>
-          </S.LinkItem>
-        </S.Links>
-      </S.Navigation>
-      <S.Phone href="tel:88003335599">8 (800) 333-55-99</S.Phone>
-    </S.HeaderWrapper>
-  </S.StyledHeader>
-);
+            <S.LinkItem>
+              <S.Link
+                $isActiveLink={location.pathname === AppRoute.Special}
+                to={AppRoute.Special}
+              >
+                {Pages.Special}
+              </S.Link>
+            </S.LinkItem>
+
+            <S.LinkItem>
+              <S.Link
+                $isActiveLink={location.pathname === AppRoute.Contacts}
+                to={AppRoute.Contacts}
+              >
+                {Pages.Contacts}
+              </S.Link>
+            </S.LinkItem>
+          </S.Links>
+        </S.Navigation>
+        <S.Phone href="tel:88003335599">8 (800) 333-55-99</S.Phone>
+      </S.HeaderWrapper>
+    </S.StyledHeader>
+  )
+};
 
 export default Header;
